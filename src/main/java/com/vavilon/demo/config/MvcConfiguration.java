ee9cfc0cf276560be2b752046ee0cab4e05f7268
Roman <rlukashenko@cygno.com>
2021-10-17 15:33:59 +0300

    secure cookie

--- src/main/java/com/vavilon/demo/config/MvcConfiguration.java
+++ src/main/java/com/vavilon/demo/config/MvcConfiguration.java
@@ -14,7 +14,7 @@ public class MvcConfiguration implements WebMvcConfigurer {
     public TomcatContextCustomizer sameSiteCookiesConfig() {
         return context -> {
             final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
-            cookieProcessor.setSameSiteCookies(SameSiteCookies.LAX.getValue());
+            cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
             context.setCookieProcessor(cookieProcessor);
         };
     }
